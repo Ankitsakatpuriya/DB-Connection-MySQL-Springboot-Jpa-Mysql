65d88ec5a97e4ea4794be1f9fef0dee8bdf7855c
Ankit <ankitsakatpuriya02@email.com>
2021-12-09 10:33:03 +0530

    log.info add

--- src/main/java/com/mkyong/HelloRestController.java
+++ src/main/java/com/mkyong/HelloRestController.java
@@ -1,14 +1,20 @@
 package com.mkyong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
-	
+
+	private static final Logger log = LoggerFactory.getLogger(HelloRestController.class);
+
 	@GetMapping("/")
 	public String hello() {
-	  return "Hello & Welcome to Ankit !!!";
+		log.info("Database Connection Successfully");
+		return "Hello & Welcome to Ankit Database Connection Successful !!!";
+
 	}
 
 }
